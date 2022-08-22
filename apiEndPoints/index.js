689df3126d7f92edfeffc586ca511fd34d0cd20d
Alex <alexandre.dutay@outlook.fr>
2022-08-22 15:09:41 +0200

    correct fix

--- apiEndPoints/index.js
+++ apiEndPoints/index.js
@@ -1,7 +1,7 @@
 
 let url 
 if(process.env.NODE_ENV === 'development') url = 'http://localhost:3000/api'
-if(process.env.NODE_ENV === 'production') url = 'https://spreadsheet-chi.vercel.app'
+if(process.env.NODE_ENV === 'production') url = 'https://spreadsheet-chi.vercel.app/api'
 
 
 export const apiEndPoints = {
